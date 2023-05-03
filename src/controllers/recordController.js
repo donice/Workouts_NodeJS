406fad92106962fd553bb30d8288f20940f23391
Donice <technocrater.pdevelopers@gmail.com>
2023-05-03 14:24:18 +0100

    added

---
+++ src/controllers/recordController.js
@@ -0,0 +1,10 @@
+// const recordService = require("../services/recordService");
+// const { getAllWorkouts } = require("./workoutController");
+// const require = getAllWorkouts
+
+// const getRecordForWorkout = (req, res) => {
+//     const allWorkouts = req.query.allWorkouts
+//     console.log(allWorkouts)
+// };
+
+// module.export = { getRecordForWorkout };
